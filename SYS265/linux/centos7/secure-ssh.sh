ebcc6a9c3c530a60e914de2845b4e802de41c11b
lkaine24 <lucas.kaine@mymail.champlain.edu>
2020-02-20 13:32:01 -0500

    Harden ssh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,12 +3,10 @@
 #author lkaine24
 #creates a new ssh user using $1 parameter
 #adds a public key from the llocal repo or curled from the remote repo
-user=$1
-useradd -m -d /home/$user -s /bin/bash $user
-mkdir /home/$user/.ssh
-mkdir /home/$user/.ssh/authorized_keys
-cp /home/lucas/Tech-Journal/SYS265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
-sudo chmod 700 /home/$user/.ssh
-sudo chmod 600 /home/$user/.ssh/authorized_keys
-sudo chown -R $user:$user /home/$user/.ssh
+#user=$1
+#useradd -m -d /home/$user -s /bin/bash $user
+#mkdir /home/$user/.ssh
+#mkdir /home/$user/.ssh/authorized_keys
+#cp /home/lucas/Tech-Journal/SYS265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
+sed -z 's/PermitRottLogin yes\|$/PermitRootLogin no/' /etc/ssh/sshd_config
 
