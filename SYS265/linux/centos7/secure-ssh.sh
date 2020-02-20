f54818ea8e98270049019eeda2aa0c310d61f784
lkaine24 <lkaine24@gmail.com>
2020-02-20 19:07:20 +0000

    Hardened Script

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+#secure-ssh.sh
+#author lkaine24
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+user=$1
+sudo useradd -m -d /home/$user -s /bin/bash $user
+sudo mkdir /home/$user/.ssh
+sudo cp /home/lucas/Tech-Journal/SYS265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
+sudo chmod 700 /home/$user/.ssh
+sudo chmod 600 /home/$user/.ssh/authorized_keys
+sudo chown -R $user:$user /home/$user/.ssh
