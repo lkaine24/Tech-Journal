e8261ed9560d9f085b17782c0de67495ccefc61c
lkaine24 <lkaine24@gmail.com>
2020-02-23 20:52:34 +0000

    ssh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,9 +3,10 @@
 #author lkaine24
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
-#removes roots ability to ssh in
 user=$1
 sudo useradd -m -d /home/$user -s /bin/bash $user
 sudo mkdir /home/$user/.ssh
 sudo cp /home/lucas/Tech-Journal/SYS265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
-sed -z 's/PermitRootLogin yes\|$/PermitRootLogin no/' /etc/ssh/sshd_config
+sudo chmod 700 /home/$user/.ssh
+sudo chmod 600 /home/$user/.ssh/authorized_keys
+sudo chown -R $user:$user /home/$user/.ssh
