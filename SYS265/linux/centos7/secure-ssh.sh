977c9232bac9b11bec3c940ab508d9c219d11942
lkaine24 <lkaine24@gmail.com>
2020-02-23 20:47:20 +0000

    ssh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -7,6 +7,4 @@ user=$1
 sudo useradd -m -d /home/$user -s /bin/bash $user
 sudo mkdir /home/$user/.ssh
 sudo cp /home/lucas/Tech-Journal/SYS265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
-sudo chmod 700 /home/$user/.ssh
-sudo chmod 600 /home/$user/.ssh/authorized_keys
-sudo chown -R $user:$user /home/$user/.ssh
+sed -z 's/PermitRootLogin yes\|$/PermitRootLogin no/' /etc/ssh/sshd_config
